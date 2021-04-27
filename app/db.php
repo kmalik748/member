9129a1bfacce386057e2f4bc8fab1895967eacc8
Kashif Ali <kmalik748@gmail.com>
2021-04-27 16:53:55 +0500

    Apr 27, 4:53 PM

--- app/db.php
+++ app/db.php
@@ -1,6 +1,5 @@
 <?php
-//$con = mysqli_connect("localhost","root","kashif@123","project_member");
-$con = mysqli_connect("localhost","root","","member");
+$con = mysqli_connect("localhost","root","kashif@123","project_member");
 //$con = mysqli_connect("localhost","root","","member");
 
 // Check connection
